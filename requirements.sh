aee3515c8f96087d03e9516a0f1314cbde71f83a
Dharanidaran <102400834+dharani003@users.noreply.github.com>
2024-12-01 23:25:27 +0530

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install aircrack-ng suite
+sudo apt update
+sudo apt install aircrack-ng
+
+# Install cowsay
+sudo apt install cowsay
+
+# Install lolcat
+sudo apt install lolcat
+
+# Install toilet
+sudo apt install toilet
+
